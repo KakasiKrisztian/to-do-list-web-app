f66543f6295a37a0fda3e79f30bda7fa1ea7615f
Krisztian Kakasi <cristy11_1998@yahoo.com>
2019-11-11 20:52:34 +0200

    added support for creating new items

--- js/to-do-list.js
+++ js/to-do-list.js
@@ -14,6 +14,26 @@ window.ToDoList = {
         });
     },
 
+    createItem: function () {
+        let descriptionValue = $("#description-field").val();
+        let deadlineValue = $("#deadline-field").val();
+
+        var requestBody = {
+            description: descriptionValue,
+            deadline: deadlineValue
+        };
+        $.ajax({
+            url: ToDoList.API_URL,
+            method: "POST",
+            //MIME type
+            contentType: "application/json",
+            data: JSON.stringify(requestBody)
+        }).done(function () {
+            ToDoList.getItems();
+        })
+
+    },
+
     displayItems: function (items) {
         var tableContent = "";
 
@@ -32,9 +52,17 @@ window.ToDoList = {
             <td><input type="checkbox" class="mark-done" data-id="${item.id}" ${checkedAttribute}/></td>
             <td><a href="#" class="delete-item" data-id="${item.id}"><i class="fas fa-dumpster"></i></a></td>
         </tr>`
+    },
+
+    bindEvents: function () {
+        $("#create-item-form").submit(function (event) {
+            event.preventDefault();
+
+            ToDoList.createItem();
+        })
     }
 
 
-
 };
-ToDoList.getItems()
+ToDoList.getItems();
+ToDoList.bindEvents();
